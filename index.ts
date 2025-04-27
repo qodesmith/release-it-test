dc29be2ff80bdd1b84465674cf0222366214ddd4
Qodesmith <theqodesmith@gmail.com>
2025-04-27 07:22:05 -0400

    Checking for tabs in all files

--- index.ts
+++ index.ts
@@ -1,5 +1,27 @@
 /** biome-ignore-all lint/suspicious/noConsole: test project */
 
-export function something() {
-  console.log('my module does something!')
+import fs from 'node:fs'
+
+function containsTab() {
+  const dirContents = fs.readdirSync('.', {
+    withFileTypes: true,
+    encoding: 'utf8',
+    recursive: true,
+  })
+
+  dirContents.forEach(dirent => {
+    if (
+      !(
+        dirent.parentPath.includes('node_modules') ||
+        dirent.parentPath.includes('.git')
+      ) &&
+      dirent.isFile()
+    ) {
+      const {name, parentPath} = dirent
+      const hasTab = fs.readFileSync(`./${parentPath}/${name}`).includes('\t')
+      console.log(name, hasTab)
+    }
+  })
 }
+
+containsTab()
