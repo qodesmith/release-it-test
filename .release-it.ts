7b9814b6dd4993383b33de04cf297b556135933b
Qodesmith <theqodesmith@gmail.com>
2025-04-29 06:36:15 -0400

    Updated release-it settings

--- .release-it.ts
+++ .release-it.ts
@@ -2,4 +2,16 @@ import type {Config} from 'release-it'
 
 // These are only option overrides. Default options will take effect.
 // biome-ignore lint/style/noDefaultExport: release-it expects a default export
-export default {} satisfies Config
+export default {
+  git: {
+    changelog:
+      'npx auto-changelog --stdout --commit-limit false -u --template https://raw.githubusercontent.com/release-it/release-it/main/templates/changelog-compact.hbs',
+  },
+  hooks: {
+    'before:init': ['bun run check'],
+    'after:bump': 'npx auto-changelog -p',
+  },
+  npm: {
+    publish: false,
+  },
+} satisfies Config
