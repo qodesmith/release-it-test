9da38e52935137613c815333989b97de22d33f1d
Qodesmith <theqodesmith@gmail.com>
2025-04-29 06:29:59 -0400

    Added release-it

---
+++ .release-it.ts
@@ -0,0 +1,5 @@
+import type {Config} from 'release-it'
+
+// These are only option overrides. Default options will take effect.
+// biome-ignore lint/style/noDefaultExport: release-it expects a default export
+export default {} satisfies Config
